2c49b12aee8fa696a468a39b461a97527b733b61
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-09-24 16:37:23 +0200

    feat: add kapa.ai integration (#207)
    Evaluate kapa.ai service for a trial period

--- conf.py
+++ conf.py
@@ -60,6 +60,7 @@ html_theme = 'sphinx_book_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = ["custom.css"]
+html_js_files = ['kapa.js']
 
 html_theme_options = {
     "repository_url": "https://github.com/NethServer/nethsecurity-docs",
