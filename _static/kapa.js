808fcb2dc549f94fc82588959290bcfb1d8c3d89
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-09-24 11:40:25 +0200

    feat: add kapa.it integration

---
+++ _static/kapa.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var script = document.createElement("script");
+  script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+  script.setAttribute("data-website-id", "96f18081-6bb0-4654-8734-d5b4c774d5c5");
+  script.setAttribute("data-project-name", "NethSecurity");
+  script.setAttribute("data-project-color", "#7490ff");
+  script.setAttribute("data-project-logo", "https://nethsecurity-docs--207.org.readthedocs.build/en/207/_static/favicon.ico");
+  script.setAttribute("data-button-position-bottom", "120px");
+  script.async = true;
+  document.head.appendChild(script);
+});
