501e6c9d07bb587911e139b50e9331b57514c2dd
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-10-03 08:54:47 +0200

    fix(kapa): fix logo url

--- _static/kapa.js
+++ _static/kapa.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   script.setAttribute("data-website-id", "96f18081-6bb0-4654-8734-d5b4c774d5c5");
   script.setAttribute("data-project-name", "NethSecurity");
   script.setAttribute("data-project-color", "#000000");
-  script.setAttribute("data-project-logo", "https://nethsecurity-docs--207.org.readthedocs.build/en/207/_static/favicon.ico");
+  script.setAttribute("data-project-logo", "https://docs.nethsecurity.org/en/latest/_static/favicon.ico");
   script.setAttribute("data-button-position-bottom", "120px");
   script.setAttribute("data-source-group-ids-include", "5ea2f7fd-9fe2-405b-baa2-5682c38d654a");
   script.setAttribute("data-modal-disclaimer", "This is an AI bot that will give you answers only about **NethSecurity 8**. Older NethServer or NethSecurity versions are not supported.");
